2be2e5c0e70a69b947343ac52d4a4aa26ea06c76
jonathan-veliz <joniveliz1993@gmail.com>
2023-09-28 20:35:26 -0300

    Update ListaPalabra.jsx
    importacion de agregar palabra

--- src/ListaPalabra.jsx
+++ src/ListaPalabra.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react'
-import { AgregarPalabras } from '../componentes/AgregarPalabras'
+import { AgregarPalabras } from './componentes/AgregarPalabras'
 
 export const ListaPalabra = () => {
  
